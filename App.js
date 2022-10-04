ee4f1a7526731f9f4bae256e549ed36e02b7f830
MatthieuBruh <matthieu.bruhwiler@etu.hesge.ch>
2022-10-04 15:03:19 +0300

    Exercise done

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
+import react, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import * as Speech from 'expo-speech';
 
 export default function App() {
+  const [userEntry, setUserEntry] = useState('');
+
+  const readText = () => {
+    if (userEntry !== '') {
+      Speech.speak(userEntry);
+      setUserEntry('')
+    } else {
+      Speech.speak('Please enter some text to read');
+      alert('Please enter some text to read');
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={{ width: '75%', height: 40, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={text => setUserEntry(text)}
+        value={userEntry}
+      />
+
+      <Button
+        title="PRESS TO HEAR TEXT"
+        onPress={readText}
+      />
       <StatusBar style="auto" />
     </View>
   );
